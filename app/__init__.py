1f38678f7c0492323fdee232d24ac6d7cb582715
Farhan kamil Hermansyah <affschannel@gmail.com>
2026-05-04 10:11:50 +0700

    feat: add flask-login authentication (login, register, logout)

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy() ## MEMANGGIL DB
+db = SQLAlchemy() # INFO = MEMANGGIL DB
+login_manager = LoginManager() # INFO = MEMANGGIL LOGIN MANAGER
 
-def create_app(config_class=Config): ## FILE CONFIG FIJADIIN CONFIG UNTUK FILE .ENV
+def create_app(config_class=Config): # INFO = FILE CONFIG FIJADIIN CONFIG UNTUK FILE .ENV
     app = Flask(__name__)
-    app.config.from_object(config_class) ## CONFIG DITERAPKAMN
+    app.config.from_object(config_class) # INFO = CONFIG DITERAPKAMN
     
-    db.init_app(app) ## MENYAMBUNGKAN DB KE FLASK
+    db.init_app(app) # INFO = MENYAMBUNGKAN DB KE FLASK
+    
+    login_manager.init_app(app)
+    login_manager.login_view = 'login' # INFO = route login
+    login_manager.login_message = "Silakan login terlebih dahulu untuk mengakses halaman ini."
     
     with app.app_context():
         from . import models
         
-        db.create_all() ## MEMBUAT COLUMN DATABASE JIKA BELUM ADA
-    
+        @login_manager.user_loader
+        def load_user(user_id):
+            return models.User.query.get(int(user_id))
+        
+        db.create_all() # INFO = MEMBUAT COLUMN DATABASE JIKA BELUM ADA
+
+        from . import routes
     return app
